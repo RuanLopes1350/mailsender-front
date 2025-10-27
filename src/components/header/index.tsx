99a100a305bbb29b034fd12fc0006877bf8e5ff1
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-10-27 00:38:19 -0400

    first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="/logo.png" alt="Logo" />
+            <h1>Mail Sender</h1>
+            <p>Painel Administrativo</p>
+        </header>
+    )
+}
