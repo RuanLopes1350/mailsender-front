7b5cd1fe6cd1e986db90e712d0e0113af58f4d3c
Ruan Lopes <romeu.unidac.alpha.nautilos@gmail.com>
2025-10-29 18:40:33 -0400

    iniciado tentativa de switch em tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Tab from "@/components/tab";
 export default function Home() {
   return (
     <>
-      <Tab icon='/dashboard-white.png' text='Dashboard' />
-      <Tab icon='keys-white.png' text='API Keys'/>
-      <Tab icon='email-white.png' text='Testar Email'/>
-      <Tab icon='logs-white.png' text='Logs Recentes'/>
+      <Tab icon='/dashboard' text='Dashboard' />
+      <Tab icon='keys' text='API Keys' />
+      <Tab icon='email' text='Testar Email' />
+      <Tab icon='logs' text='Logs Recentes' />
     </>
   );
 }
--- src/components/tab/index.tsx
+++ src/components/tab/index.tsx
@@ -1,15 +1,34 @@
+"use client";
+
 interface TabProps {
     icon?: string;
     text: string;
+    selected: boolean;
 }
 
-export default function Tab({ icon, text }: TabProps) {
+export default function Tab({ icon, text, selected }: TabProps) {
+    const handleClick = () => {
+        console.log(`Tab ${text} clicked`);
+        selected = true;
+    }
+    if (selected) {
+        return (
+            <div className="bg-[#4F46E5] h-[58.8px] rounded-[10px] flex items-center justify-center gap-2 px-4 w-[318.15px]" onClick={handleClick}>
+                {icon && (
+                    <img src={`${icon}-white.png`} />
+                )}
+                <span>{text}</span>
+            </div>
+        )
+    }
+    
     return (
-        <div className="bg-[#4F46E5] h-[58.8px] rounded-[10px] flex items-center justify-center gap-2 px-4 w-[318.15px] cursor-pointer hover:bg-[#2c21c0] transition-colors">
+        <div className="bg-[#ffffff] h-[58.8px] rounded-[10px] flex items-center justify-center gap-2 px-4 w-[318.15px] cursor-pointer hover:text-white hover:bg-[#aca8e1] transition-colors" onClick={handleClick}>
             {icon && (
-                <img src={icon}/>
+                <img src={`${icon}-black.png`} />
             )}
-            <span>{text}</span>
+            <span className="text-[#374151]">{text}</span>
         </div>
     )
+
 }
